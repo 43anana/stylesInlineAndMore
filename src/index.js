2fc559041573a5d6ddd8f423ad9cc8d13c427b6a
43anana <75275624+43anana@users.noreply.github.com>
2021-04-02 19:48:29 +0200

    idk

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+//REMEMBER: inside objects only commas
+//hier brauchen wir auch strings
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "2px solid grey"
+};
+
+customStyle.color = "orange";
+
+ReactDOM.render(
+  <h1 style={customStyle}>laleluuu</h1>,
+  document.getElementById("root")
+);
